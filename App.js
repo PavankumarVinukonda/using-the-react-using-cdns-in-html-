d7e82e475d9353c673ef3fe68cf0bb78e5b08cff
pavantechally <pavankumar.v@techallylabs.com>
2023-10-08 10:38:43 +0530

    added the changes

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 // const heading = React.createElement("h1",{id:"head"},"hai abhi");
+import React from 'react'
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:'child'},
 
-[React.createElement("h1",{id:"head"},"hello abhi from react"),React.createElement("h1",{id:"head"},"hello abhi from react"),React.createElement("h1",{id:"head"},"hello abhi from react")]),
-React.createElement("div",{id:'child1'},
 
-[React.createElement("h1",{id:"head"},"hello abhi from react"),React.createElement("h1",{id:"head"},"hello abhi from react"),React.createElement("h1",{id:"head"},"hello abhi from react")])
-])
-    
+// const parent = React.createElement("div",{id:"parent"},[
+//     React.createElement("div",{id:'child'},
+
+// [React.createElement("h1",{id:"head"},"hello abhi from react"),React.createElement("h1",{id:"head"},"hello abhi from react"),React.createElement("h1",{id:"head"},"hello abhi from react")]),
+// React.createElement("div",{id:'child1'},
+
+// [React.createElement("h1",{id:"head"},"hello abhi from react"),React.createElement("h1",{id:"head"},"hello abhi from react"),React.createElement("h1",{id:"head"},"hello abhi from react")])
+// ])
+  
+
+const jsxElement = <h1 id="heading" >hello abhi from react </h1>
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root?.render(parent)
+    root?.render(jsxElement)
